04da0f08620615ea50e8dce28e8c8cc1c3cccc5f
childrem <childrem@oregonstate.edu>
2019-07-09 22:36:17 -0700

    created asserttrue and test structure

--- projects/childrem/dominion/unittest1.c
+++ projects/childrem/dominion/unittest1.c
@@ -1,37 +1,84 @@
-//#include "dominion.h"
-//#include "dominion_helpers.h"
+#include "dominion.h"
+#include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
 #include <assert.h>
-//#include "rngs.h"
+#include "rngs.h"
 
 
 // set NOISY_TEST to 0 to remove printfs from output
 #define NOISY_TEST 1
 
+void asserttrue(int actualValue, int expectedValue, char* valueName) {
+
+	if (actualValue == expectedValue) {
+
+		printf("\nPASS: Actual value of %s was %d and expected value was %d!\n", 
+			valueName, actualValue, expectedValue);
+	}
+
+	else {
+
+		printf("\nFAIL: VALUES of %s NOT EQUAL! Actual value: %d, Expected value: %d\n", 
+			valueName, actualValue, expectedValue);
+	}
+
+}
+
+
 int main() {
-	/*int i;
+	int i;
 	int seed = 1000;
 	int numPlayer = 2;
 	int maxBonus = 10;
 	int p, r, handCount;
 	int bonus;
-	int k[10] = { adventurer, council_room, feast, gardens, mine
+	int k[10] = { minion, ambassador, tribute, gardens, mine
 			   , remodel, smithy, village, baron, great_hall };
-	struct gameState G;
+	struct gameState G, beforeFunction;
 	int maxHandCount = 5;
-	// arrays of all coppers, silvers, and golds
-	int coppers[MAX_HAND];
-	int silvers[MAX_HAND];
-	int golds[MAX_HAND];
-	for (i = 0; i < MAX_HAND; i++)
-	{
-		coppers[i] = copper;
-		silvers[i] = silver;
-		golds[i] = gold;
-	}
 
-	printf("TESTING updateCoins():\n");
+	int choice1;
+	int currentPlayer;		// 0 will be player 1 and 1 will be player 2
+	int retValue;		// return value for function being tested
+
+	// arrays of all coppers, silvers, and golds
+	//int coppers[MAX_HAND];
+	//int silvers[MAX_HAND];
+	//int golds[MAX_HAND];
+	//for (i = 0; i < MAX_HAND; i++)
+	//{
+		//coppers[i] = copper;
+		//silvers[i] = silver;
+		//golds[i] = gold;
+	//}
+
+	printf("TESTING baronEffect():\n");
+
+
+	// TEST 1 -- choice1 is set -- we should be discarding an estate and adding 4 gold to currentPlayer
+
+	// initialze game
+	memset(&G, 23, sizeof(struct gameState));
+	memset(&beforeFunction, 23, sizeof(struct gameState));
+
+	r = initializeGame(numPlayer, k, seed, &G);
+
+	// capture initial state of the game
+	memcpy(&beforeFunction, &G, sizeof(struct gameState));
+
+	// run function to test
+
+	
+
+	// TEST 2 -- choice1 is NOT set -- currentPlayer should be gaining an estate (coins should remain the same)
+
+
+
+
+	//asserttrue(5, 6, "coins");
+
+	/*
 	for (p = 0; p < numPlayer; p++)
 	{
 		for (handCount = 1; handCount <= maxHandCount; handCount++)
@@ -68,7 +115,9 @@ int main() {
 		}
 	}
 
-	printf("All tests passed!\n");
-*/
+	*/
+
+	printf("\nEnd of Unit Test!\n");
+
 	return 0;
 }
