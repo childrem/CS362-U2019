a66a176ca27204c4f34ee69bc47b756c7d532072
childrem <childrem@oregonstate.edu>
2019-07-10 18:19:26 -0700

    added more baron tests

--- projects/childrem/dominion/unittest1.c
+++ projects/childrem/dominion/unittest1.c
@@ -58,7 +58,7 @@ int main() {
 	//}
 
 	printf("TESTING baronEffect():\n");
-
+	printf("\nFirst test has choice1 set\n\n");
 
 	// TEST 1 -- choice1 is set -- we should be discarding an estate and adding 4 coins to currentPlayer
 
@@ -72,6 +72,7 @@ int main() {
 
 	// We need to make sure that each player has at least 1 estate card in their first hand for testing
 	// Set other hand positions to copper for better control
+	// Last hand position needs to be open so we can gain an estate card if necessary
 
 	for (int c = 0; c < numPlayer; c++) {
 		tempNumEstatesBefore = numEstatesBefore;
@@ -86,6 +87,10 @@ int main() {
 				G.hand[c][handPosition] = copper;
 			}
 		}
+
+		// free up last position to gain an estate if needed
+
+		//discardCard(G.hand[c][maxHandCount-1], c, &G, 0);
 
 	}
 
@@ -130,9 +135,104 @@ int main() {
 	}
 
 
+	// Check to see if the discard count increased by 1 and that the last position of the discard array is an estate
+
+	asserttrue(G.discardCount[currentPlayer], beforeFunction.discardCount[currentPlayer], "Current Player Discard Count");
+
+	asserttrue(G.discard[currentPlayer][G.discardCount[currentPlayer]-1], estate, 
+		"Newest discard card (Would be 1 for estate If this Passes it means code FAILED):");
+
+
+	// Make sure the other player's discard pile stayed the same
+
+	asserttrue(G.discardCount[currentPlayer + 1], beforeFunction.discardCount[currentPlayer + 1], 
+		"Other Player Discard Count");
+
+
+
 	// TEST 2 -- choice1 is NOT set -- currentPlayer should be gaining an estate (coins should remain the same)
 
+	printf("\n\nNow testing with choice1 NOT set!\n\n");
 
+	// initialze new game
+	memset(&G, 23, sizeof(struct gameState));
+	memset(&beforeFunction, 23, sizeof(struct gameState));
+
+	r = initializeGame(numPlayer, k, seed, &G);
+	G.coins = 0;			// Start at 0 in test cases for easy 
+
+
+	// We need to make sure that each player has at least 1 estate card in their first hand for testing
+	// Set other hand positions to copper for better control
+
+	for (int c = 0; c < numPlayer; c++) {
+		tempNumEstatesBefore = numEstatesBefore;
+
+		for (int handPosition = 0; handPosition < maxHandCount; handPosition++) {
+			if (tempNumEstatesBefore > 0) {
+				G.hand[c][handPosition] = estate;
+				tempNumEstatesBefore--;
+			}
+
+			else {
+				G.hand[c][handPosition] = copper;
+			}
+		}
+
+	}
+
+	// capture initial state of the game
+	memcpy(&beforeFunction, &G, sizeof(struct gameState));
+
+	// run function to test - this time don't set choice1
+	choice1 = 0;
+	currentPlayer = 0;
+
+	retValue = baronEffect(choice1, &G, currentPlayer);
+
+
+	// compare number of coins expected vs actual
+
+	asserttrue(G.coins, beforeFunction.coins, "COINS");
+
+
+	// compare the number of estate cards in the current player's hand (should be the same)
+
+	tempNumEstatesBefore = numEstatesBefore;
+
+	for (int c = 0; c < numPlayer; c++) {
+
+		numEstates = 0;
+
+		for (int handPosition = 0; handPosition < maxHandCount; handPosition++) {
+
+			if (G.hand[c][handPosition] == estate) {
+				numEstates++;
+			}
+		}
+
+		if (c == 0) {		// the current player
+			asserttrue(numEstates, tempNumEstatesBefore, "Current Player Num Estates");
+		}
+
+		else {
+			asserttrue(numEstates, tempNumEstatesBefore, "Other Player Num Estates");
+		}
+
+	}
+
+	// Check to make sure the discard count increased by 1 and that the last position of the discard array is an estate
+
+	asserttrue(G.discardCount[currentPlayer], beforeFunction.discardCount[currentPlayer] + 1, "Current Player Discard Count");
+
+	asserttrue(G.discard[currentPlayer][G.discardCount[currentPlayer] - 1], estate,
+		"Newest discard card (Should be 1 for estate)");
+
+
+	// Make sure the other player's discard pile stayed the same
+
+	asserttrue(G.discardCount[currentPlayer + 1], beforeFunction.discardCount[currentPlayer + 1],
+		"Other Player Discard Count");
 
 
 	//asserttrue(5, 6, "coins");
